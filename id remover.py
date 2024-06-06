4519deb6397ae31971e3ac9b77a7ec66362f1010
numberss <red.ace.l@outlook.com>
2024-06-07 00:02:42 +0100

    made it a bit cleaner

--- id remover.py
+++ id remover.py
@@ -1,7 +1,6 @@
-with open("D:\Code\Github Repos\\foundation-update\maps_surf.txt", encoding="utf-8") as maps:
-    for line in maps:
+with open("D:\Code\Github Repos\\foundation-update\maps_surf.txt", encoding="utf-8") as surfMaps:
+    for line in surfMaps:
         line = line.split()
         del line[0:2]
         mapName = " ".join(line)
         print(mapName)
-        
